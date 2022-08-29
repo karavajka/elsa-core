eccca88c07653b440f85069785416e1ce247d0d4
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-08-29 14:50:08 +0200

    Add couple of read wildcards for testing

--- src/modules/Elsa.Identity/Features/IdentityFeature.cs
+++ src/modules/Elsa.Identity/Features/IdentityFeature.cs
@@ -2,9 +2,7 @@ using Elsa.Common.Features;
 using Elsa.Features.Abstractions;
 using Elsa.Features.Attributes;
 using Elsa.Features.Services;
-using Elsa.Identity.Extensions;
 using Elsa.Identity.Implementations;
-using Elsa.Identity.Models;
 using Elsa.Identity.Options;
 using Elsa.Identity.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,16 +15,12 @@ public class IdentityFeature : FeatureBase
     public IdentityFeature(IModule module) : base(module)
     {
     }
-
-    public ICollection<User> Users { get; set; } = new List<User>();
+    
     public Action<IdentityOptions>? IdentityOptions { get; set; }
     public Action<TokenOptions>? TokenOptions { get; set; }
 
     public override void Apply()
     {
-        if (Users.Any())
-            Services.Configure<IdentityOptions>(options => options.Users.AddRange(Users));
-        
         Services.Configure(IdentityOptions);
         Services.Configure(TokenOptions);
         
